26d43a351a46ef99bd15603a5b1da0729c72cb06
enkil317 <87215205+enkil317@users.noreply.github.com>
2021-12-21 10:01:15 -0500

    cleaned up error components

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ if (process.env.USER) require("dotenv").config();
 
 
 const express = require("express");
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
 const moviesRouter = require("./movies/movies.router")
 const theatersRouter= require("./theaters/theaters.router")
 const reviewsRouter = require("./reviews/reviews.router")
 const app = express();
 const cors = require("cors");
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -15,16 +19,10 @@ app.use("/movies", moviesRouter)
 app.use("/reviews", reviewsRouter)
 app.use("/theaters",theatersRouter)
 
-  app.use((req, res, next) => {
-    next({ status: 404, message: `Not found: ${req.originalUrl}` });
-  });
-  
-  // Error handler
-  app.use((error, req, res, next) => {
-    console.error(error);
-    const { status = 500, message = "Something went wrong!" } = error;
-    res.status(status).json({ error: message });
-  });
+
+app.use(notFound);
+app.use(errorHandler);
+
   
 
 module.exports = app;
